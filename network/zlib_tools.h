48ed008d73107760428ab6ebcf3ef6621bf0e506
protobyter <bobrow2006@gmail.com>
2020-10-18 20:23:42 +0100

    Fixed some compile errors

--- network/zlib_tools.h
+++ network/zlib_tools.h
@@ -6,7 +6,8 @@
 #define EPISIM_ZLIB_TOOLS_H
 
 #include "../libs/zlib/zlib.h"
-
+#include "cstring"
+#include "cassert"
 #if defined(MSDOS) || defined(OS2) || defined(WIN32) || defined(__CYGWIN__)
 #  include <fcntl.h>
 #  include <io.h>
@@ -19,6 +20,7 @@
 
 #define setupDeflate
 
+// Defined in zlib_tools.cpp
 extern int deflateSerialData(char* data, char* dest, int lvl, int len);
 extern int inflateSerialData(char* data, char* dest, int lvl, int len);
 extern int deflateSerialData(char* data, int lvl, int len);
